2db80dac79f4af93c0200bb4175de8e38dc3c832
MorganHuegel <mhue2910@gmail.com>
2020-04-28 00:57:42 -0400

    updated hover styles on info boxes

--- components/landingInfoBox.js
+++ components/landingInfoBox.js
@@ -8,8 +8,20 @@ const LandingInfoBox = props => {
     <div className='info-box'>
       <Link href={href}>
         <a className='link'>
-          <div className='icon-container'>{icon}</div>
-          <h3 className='main-text'>{mainText}</h3>
+          <div className='main'>
+            <div className='main-icon'>{icon}</div>
+            <h3 className='main-text'>{mainText}</h3>
+          </div>
+
+          <div className='details'>
+            <h3><span>{icon}</span>{mainText}</h3>
+            <ul>
+            <li>Foo</li>
+            <li>Foo</li>
+            <li>Foo</li>
+            <li>Foo</li>
+            </ul>
+          </div>
         </a>
       </Link>
       <style jsx>{`
@@ -27,17 +39,47 @@ const LandingInfoBox = props => {
           height: 100%;
           color: ${styles.primaryColor};
           text-decoration: none;
+          padding: 20px;
+          transition: background-color 400ms, box-shadow 400ms;
+        }
+        .main {
+          height: 100%;
+          width: 100%;
           display: flex;
           flex-direction: column;
           justify-content: space-evenly;
           align-items: center;
         }
-        .icon-container {
+        .main-icon {
           font-size: 10rem;
         }
         .main-text {
           font-size: 2rem;
         }
+        .details {
+          display: none;
+        }
+        
+        .link:hover, .link:focus {
+          background-color: ${styles.primaryColor};
+          color: ${styles.lightGrayColor};
+          box-shadow: 0px 0px 30px 0px black;
+        }
+
+        .link:hover .main, .link:focus .main {
+          display: none;
+        }
+        .link:hover .details, .link:focus .details {
+          display: initial;
+        }
+        .details h3 {
+          font-size: 1.6rem;
+          margin-bottom: 20px;
+        }
+        .details span {
+          margin-right: 10px;
+        }
+        
         @media only screen and (min-width: ${styles.screenMd}) {
           .info-box {
             max-width: 490px;
