125af68df72d4278099ebdcbe404cfc4b4dd4a06
MorganHuegel <mhue2910@gmail.com>
2020-05-23 12:02:16 -0400

    finished styling for contact form

--- components/contactForm.js
+++ components/contactForm.js
@@ -1,53 +1,57 @@
-
+import styles from '../style'
 
 export default class ContactForm extends React.Component {
-  state = {
-    name: {
-      value: '',
-      error: ''
-    },
-    email: {
-      value: '',
-      error: ''
-    },
-    phone: {
-      value: '',
-      error: ''
-    },
-    service: {
-      value: '',
-      error: ''
-    },
-    message: {
-      value: '',
-      error: ''
-    },
+  constructor(props){
+    super(props);
+    this.state = {
+      name: {
+        value: '',
+        error: ''
+      },
+      email: {
+        value: '',
+        error: ''
+      },
+      phone: {
+        value: '',
+        error: ''
+      },
+      service: {
+        value: '',
+        error: ''
+      },
+      message: {
+        value: '',
+        error: ''
+      },
+    }
   }
 
   render(){
     return (
       <form name='contact-form' id='contact-form'>
         <div className={`group ${this.state.name.error && 'error'}`}>
-          <label for='name'>Name:</label>
+          <label for='name'>Name *</label>
           <input type='text' name='name' id='name'/>
           {this.state.name.error && <p className='error-message'>{this.state.name.error}</p>}
         </div>
 
         <div className={`group ${this.state.email.error && 'error'}`}>
-          <label for='email'>Email:</label>
+          <label for='email'>Email *</label>
           <input type='email' name='email' id='email'/>
           {this.state.email.error && <p className='error-message'>{this.state.email.error}</p>}
         </div>
 
         <div className={`group ${this.state.phone.error && 'error'}`}>
-          <label for='phone'>Phone:</label>
+          <label for='phone'>Phone *</label>
           <input type='tel' name='phone' id='phone'/>
           {this.state.phone.error && <p className='error-message'>{this.state.phone.error}</p>}
         </div>
 
         <div className={`group ${this.state.service.error && 'error'}`}>
-          <label for='service'>Requested Service:</label>
+          <label for='service'>Requested Service</label>
           <select name='service' id='service'>
+            <option value='' disabled selected></option>
             <option value='commercial'>Commercial</option>
             <option value='residential'>Residential</option>
             <option value='electrical'>Electrical</option>
@@ -56,10 +60,12 @@ export default class ContactForm extends React.Component {
         </div>
 
         <div className={`group ${this.state.message.error && 'error'}`}>
-          <label for='message'>Message (optional):</label>
-          <textarea type='tel' name='message' id='message'/>
+          <label for='message'>Message</label>
+          <textarea name='message' id='message' rows={4}/>
           {this.state.message.error && <p className='error-message'>{this.state.message.error}</p>}
         </div>
+
+        <button type='submit' disabled={this.props.isSubmitting}>Send</button>
         <style jsx>{`
           .group {
             margin-bottom: 20px;
@@ -68,9 +74,9 @@ export default class ContactForm extends React.Component {
             background-color: rgb(255, 235, 235);
           }
 
-          input, textarea, select {
-            font-size: 1.4rem;
-            padding: 5px;
+          input, textarea {
+            font-size: 1rem;
+            padding: 8px;
             width: 100%;
             border-radius: 5px;
           }
@@ -83,6 +89,23 @@ export default class ContactForm extends React.Component {
           .error-message {
             margin-top: 5px;
             color: rgb(255, 100, 100);
+          }
+
+          button {
+            width: 100%;
+            background-color: ${styles.primaryColor};
+            color: white;
+            font-size: 1rem;
+            padding: 15px;
+            border-radius: 5px;
+          }
+          button:hover, button:focus {
+            cursor: pointer;
+            background-color: rgb(37, 74, 40);
+          }
+          button[disabled], button[disabled]:hover, button[disabled]:focus {
+            background-color: ${styles.primaryColorTransparant};
+            cursor: wait;
           }
         `}</style>
       </form>
--- pages/contact.js
+++ pages/contact.js
@@ -1,39 +1,59 @@
+import { useState } from 'react'
 import MainLayout from '../layouts/main.js'
 import ContactForm from '../components/contactForm'
 import styles from '../style'
 
-const Contact = () => (
-  <MainLayout>
-    <div className='container-fluid wrapper'>
-      <div className='form-container'>
-        <h2>Contact Us</h2>
-        <p>
-          Please submit the form below for all inquiries, 
-          or email <a href='mailto:acct@comiske.com'>acct@comiske.com</a> directly.
-          We look forward to making your next project a success!
-        </p>
-        <ContactForm />
-      </div>
-    </div>
-    <style jsx>{`
-      .wrapper {
-        background-color: ${styles.primaryColor};
-        padding: 20px;
-        height: 100%;
-      }
-      .form-container {
-        margin: 0 auto;
-        max-width: 600px;
-        background-color: rgb(240, 240, 240);
-        padding: 20px;
-        border-radius: 5px;
-        color: ${styles.primaryColor};
-      }
-      .wrapper h2 {
-        font-size: 2.5rem;
-      }
-    `}</style>
-  </MainLayout>
-)
+class Contact extends React.Component {
+  state = {
+    isSubmitting: false,
+    isSuccess: false,
+    isFailure: false
+  }
+
+  render(){
+    return (
+      <MainLayout>
+        <div className='container-fluid wrapper'>
+          <div className='form-container'>
+            <h2>Contact Us</h2>
+            <p className='directions'>
+              Please submit the form below for all inquiries, 
+              or email <a href='mailto:acct@comiske.com'>acct@comiske.com</a> directly.
+              You can expect a response within 3 buisness days.
+              We look forward to making your next project a success!
+            </p>
+            <ContactForm isSubmitting={this.state.isSubmitting}/>
+          </div>
+        </div>
+        <style jsx>{`
+          .wrapper {
+            background-color: ${styles.primaryColor};
+            padding: 20px;
+            height: 100%;
+          }
+          .form-container {
+            margin: 0 auto;
+            max-width: 600px;
+            background-color: rgb(240, 240, 240);
+            padding: 20px;
+            border-radius: 5px;
+            color: ${styles.primaryColor};
+          }
+          .wrapper h2 {
+            font-size: 2.5rem;
+          }
+          .directions {
+            margin: 30px 0;
+          }
+          @media only screen and (min-width: ${styles.screenMd}) {
+            .wrapper {
+              padding: 50px;
+            }
+          }
+        `}</style>
+      </MainLayout>
+    )
+  }
+}
 
 export default Contact;
