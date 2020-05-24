bfb894bceee04110bc64e842a386d82acd21067e
MorganHuegel <mhue2910@gmail.com>
2020-05-24 15:48:03 -0400

    added language to contact Success page

--- components/contactSuccess.js
+++ components/contactSuccess.js
@@ -1,7 +1,44 @@
+import styles from '../style'
+
+const Icon = () => <i class="far fa-check-circle"></i>
+
 const ContactSuccess = () => {
   return (
-    <div>
-      <p>Submitted Successfully</p>
+    <div className='success-wrapper'>
+
+      <div className='icon'>
+        <Icon />
+      </div>
+
+      <div>
+        <h2>Thank you!</h2>
+      </div>
+
+      <div>
+        <p>Your message has been submitted. We will be in contact within 3 business days.  For immediate inquiries, please call 814-673-0233.</p>
+      </div>
+
+      <style jsx>{`
+        .success-wrapper {
+          height: calc(100vh - 300px);
+          max-height: 600px;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-around;
+        }
+        .icon {
+          font-size: 7rem;
+          text-align: center;
+        }
+        h2 {
+          font-size: 3.5rem;
+        }
+        @media only screen and (min-width: ${styles.screenMd}){
+          .success-wrapper {
+            height: calc(100vh - 400px);
+          }
+        }
+      `}</style>
     </div>
   )
 }
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -11,20 +11,20 @@ export default (req, res) => {
     to: emailTo,
     subject: `Inquiry from ${req.body.name}`,
     text: `
-      <h1>Request submitted from ${req.body.name}</h1>
-      <p>Name: ${req.body.name}</p>
-      <p>Email: ${req.body.email}</p>
-      <p>Phone: ${req.body.phone}</p>
-      <p>Service: ${req.body.service}</p>
-      <p>Message: ${req.body.message}</p>
+      Request submitted from ${req.body.name}
+      Name: ${req.body.name}
+      Email: ${req.body.email || 'n/a'}
+      Phone: ${req.body.phone || 'n/a'}
+      Service: ${req.body.service || 'n/a'}
+      Message: ${req.body.message || 'n/a'}
     `
   }, (err, info) => {
     if (!err) {
       // Add messageId and message to a database somewhere from info object
-      res.status(200).json({success: false})
+      return res.status(200).json({success: true})
     } else {
       // Add error to an error log somewhere
-      res.status(500).json({success: false})
+      return res.status(500).json({success: false})
     }
   });
 }
